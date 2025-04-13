2d78b0c07b6486facedd99a7a2f3ffde6559883c
Mark Bond <markbond615@gmail.com>
2025-04-13 12:45:55 -0500

    Added display of pacs, their donors and how much they donated

--- scripts/politicians.js
+++ scripts/politicians.js
@@ -1,23 +1,25 @@
 export const DisplayPoliticians = async () => {
     const politicians = await fetch("http://localhost:8088/politicians").then(res => res.json());
 
-    const politiciansHTML = politicians.map((politician) => {
-
+    const politicianCards = politicians.map((politician) => {
         return `
-            <article class="politicians">
-                <section class="politician">
-                    <header class="politician_name">
-                        <h1>${politician.name.first} ${politician.name.last}</h1>
-                    </header>
-                    <div class="politician_info">
-                        <div>Age: ${politician.age}</div>
-                        <div>Represents: ${politician.district}</div>
-                    </div>
-                </section>
-            </article>
-        
-        `
+            <section class="politician">
+                <header class="politician_name">
+                    <h1>${politician.name.first} ${politician.name.last}</h1>
+                </header>
+                <div class="politician_info">
+                    <div>Age: ${politician.age}</div>
+                    <div>Represents: ${politician.district}</div>
+                </div>
+            </section>
+        `;
     }).join("");
+
+    const politiciansHTML = `
+        <article class="politicians">
+            ${politicianCards}
+        </article>
+    `;
 
     return politiciansHTML;
 }
