74c73706913a1a80d7258c48ef314b1ada707a1c
Mark Bond <markbond615@gmail.com>
2025-04-13 09:17:16 -0500

    Added display of corporations' name and address

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import { DisplayCorporations } from "./corporations.js";
 import { DisplayPoliticians } from "./politicians.js";
 
 const container = document.querySelector("#container");
@@ -5,11 +6,12 @@ const container = document.querySelector("#container");
 const render = async () => {
 
     const politicans = await DisplayPoliticians();
+    const corporations = await DisplayCorporations();
 
     const composedHTML = `
         <h1>Honest Abe</h1>
         ${politicans}
-
+        ${corporations}
     `
 
     container.innerHTML = composedHTML;
